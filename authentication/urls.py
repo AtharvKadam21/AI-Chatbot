15385dcf2403277e0867527e1acd80ae28035023
atharvkadam21 <atharvkadam82@gmail.com>
2023-09-08 00:39:24 +0530

    signup and login done

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
     path('signup', views.signup, name='signup'),
     path('login', views.login, name='login'),
-    path('logout', views.logout, name='logout'),
-]
+    path('signout', views.signout, name='signout'),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- authentication/views.py
+++ authentication/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth.models import User
 from django.contrib import messages
-from django.contrib.auth import authenticate, login as dj_login
+from django.contrib.auth import authenticate, login as dj_login, logout
 
 
 # Create your views here.
@@ -13,6 +13,8 @@ def signup(request):
     
     if request.method == 'POST':
         username = request.POST['username']
+        fname = request.POST['fname']
+        lname = request.POST['lname']
         email = request.POST['email']
         pass1 = request.POST['pass1']
         pass2 = request.POST['pass2']
@@ -38,7 +40,7 @@ def login(request):
         if user is not None:
             dj_login(request, user)
             fname = user.username
-            return render(request, 'authentication/index.html', {'fname': username})
+            return render(request, 'authentication/index.html', {'fname': fname})
 
         else:
             messages.error(request, 'Bad credentials')
@@ -46,5 +48,7 @@ def login(request):
 
     return render(request, 'authentication/login.html')
 
-def logout(request):
-    pass
+def signout(request):
+    logout(request)
+    messages.success(request, 'logged out successfully')
+    return redirect('home')
